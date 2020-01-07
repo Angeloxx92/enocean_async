5c4fc989dd9fdad456250a27c565c13ea2cf7f4a
Angelo Cutaia <48996402+Angeloxx92@users.noreply.github.com>
2020-01-07 02:14:33 +0100

    Bump to version 1.0.2

--- setup.py
+++ setup.py
@@ -9,10 +9,12 @@ with open("README.md", "r") as fh:
 
 setup(
     name='enocean_async',
-    version='1.0.1',
-    description='EnOcean serial protocol async implementation',
+    version='1.0.2',
     author='Angelo Cutaia',
     author_email='angeloxx92@hotmail.it',
+    description='EnOcean serial protocol async implementation',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/Angeloxx92/enocean_async',
     packages=[
         'enocean_async',
@@ -30,6 +32,10 @@ setup(
         'pyserial-asyncio>=0.4',
         'beautifulsoup4>=4.3.2',
         'aioconsole>=0.1.15',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
     ],
     python_requires='>=3.7',
     )
